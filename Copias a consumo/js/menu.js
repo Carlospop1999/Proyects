0068289842e17e128167a68ff0426b8aa4be644a
Carlospop1999 <139171513+Carlospop1999@users.noreply.github.com>
2025-02-25 12:12:18 -0600

    Add files via upload

---
+++ Copias a consumo/js/menu.js
@@ -0,0 +1,21 @@
+(function() {
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+    const menuLinks = menu.querySelectorAll('.nav__links');
+  
+    openButton.addEventListener('click', () => {
+      menu.classList.add('nav__link--show');
+    });
+  
+    closeMenu.addEventListener('click', () => {
+      menu.classList.remove('nav__link--show');
+    });
+  
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        menu.classList.remove('nav__link--show');
+      });
+    });
+  })();
+  
